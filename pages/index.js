4b5a45cc5762298fd1f66a419df0d5c987f57b8a
useholt <useholt.use@gmail.com>
2025-04-28 16:52:45 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Mural() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>MEU MURAL DE SONHOS</h1>
+      <p>Formulário e lista de sonhos virão aqui!</p>
+    </div>
+  );
+}
