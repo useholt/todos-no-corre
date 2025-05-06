41f34456f1e38c0d1ef6ac47a100f7e142e3818b
useholt <useholt.use@gmail.com>
2025-05-06 13:51:27 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -162,9 +162,9 @@ export default function Mural() {
             {/* Container do texto na área branca */}
             <div style={{ 
               position: 'absolute',
-              top: '50%',
+              top: '70%',
               left: '50%',
-              transform: 'translate(-50%, -50%)',
+              transform: 'translate(-70%, -30%)',
               width: '80%',
               background: 'rgba(255, 255, 255, 0.9)',
               borderRadius: '8px',
